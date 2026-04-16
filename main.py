fa1a5e2590481102442c52e88d1c43e5eeb651c5
Alexandru <lexyus22@gmail.com>
2026-04-16 11:13:08 +0200

    Commit 4 -> Last changes

--- main.py
+++ main.py
@@ -27,24 +27,33 @@ scissors = '''
 ---.__(___)
 '''
 
-print('Use --> 0 for Rock <--, --> 1 for Paper <--, --> 2 for Scissors <--\n')
-user_nr=int(input('Enter your choice: '))
-random_nr=random.randint(0,2)
+print('Use --> 0 for Rock <--, --> 1 for Paper <--, --> 2 for Scissors <--')
+
+user_nr = int(input('Enter your choice: '))
+random_nr = random.randint(0,2)
+
 joc=[rock, paper, scissors]
 
-if random_nr==user_nr:
-    print("It's a draw! ",joc[user_nr],'Computer: ',joc[random_nr])
-elif random_nr==0 and user_nr==1:
-    print('You win! ',joc[user_nr],'Computer: ',joc[random_nr])
-elif random_nr==0 and user_nr==2:
-    print('You lose! ',joc[user_nr],'Computer: ',joc[random_nr])
-elif random_nr==1 and user_nr==0:
-    print('You lose! ',joc[user_nr],'Computer: ',joc[random_nr])
-elif random_nr==1 and user_nr==2:
-    print('You win! ',joc[user_nr],'Computer: ',joc[random_nr])
-elif random_nr==2 and user_nr==0:
-    print('You win! ',joc[user_nr],'Computer: ',joc[random_nr])
-elif random_nr==2 and user_nr==1:
-    print('You lose! ',joc[user_nr],'Computer: ',joc[random_nr])
+if random_nr == user_nr:
+    print(" ***** --> It's a draw. <-- ***** ")
+    print('Your choice: ',joc[user_nr],'Computer: ',joc[random_nr])
+elif random_nr == 0 and user_nr == 1:
+    print(' ***** --> You win. <-- ***** ')
+    print('Your choice: ',joc[user_nr],'Computer: ',joc[random_nr])
+elif random_nr == 0 and user_nr == 2:
+    print(' ***** --> You lose. <-- ***** ')
+    print('Your choice: ',joc[user_nr],'Computer: ',joc[random_nr])
+elif random_nr == 1 and user_nr == 0:
+    print(' ***** --> You lose. <-- ***** ')
+    print('Your choice: ',joc[user_nr],'Computer: ',joc[random_nr])
+elif random_nr == 1 and user_nr == 2:
+    print(' ***** --> You win. <-- ***** ')
+    print('Your choice: ',joc[user_nr],'Computer: ',joc[random_nr])
+elif random_nr == 2 and user_nr == 0:
+    print(' ***** --> You win. <-- ***** ')
+    print('Your choice: ',joc[user_nr],'Computer: ',joc[random_nr])
+elif random_nr == 2 and user_nr == 1:
+    print(' ***** --> You lose. <-- ***** ')
+    print('Your choice: ',joc[user_nr],'Computer: ',joc[random_nr])
 else:
     print('You choose an invalid number. You lose! ')
