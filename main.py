0aa1fea221e66318d494e3f441bcd2dbd497117e
Alexandru <lexyus22@gmail.com>
2026-04-16 11:02:53 +0200

    Commit 3 -> First change

--- main.py
+++ main.py
@@ -1,23 +1,50 @@
 import random
 
-print('Use --> 0 for Rock <--, --> 1 for Paper <--, --> 2 for Scissors <--')
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
 
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+print('Use --> 0 for Rock <--, --> 1 for Paper <--, --> 2 for Scissors <--\n')
 user_nr=int(input('Enter your choice: '))
 random_nr=random.randint(0,2)
+joc=[rock, paper, scissors]
 
 if random_nr==user_nr:
-    print("It's a draw! ",'Computer choose: ', random_nr)
+    print("It's a draw! ",joc[user_nr],'Computer: ',joc[random_nr])
 elif random_nr==0 and user_nr==1:
-    print('You win! ','Computer choose: ', random_nr)
+    print('You win! ',joc[user_nr],'Computer: ',joc[random_nr])
 elif random_nr==0 and user_nr==2:
-    print('You lose! ','Computer choose: ', random_nr)
+    print('You lose! ',joc[user_nr],'Computer: ',joc[random_nr])
 elif random_nr==1 and user_nr==0:
-    print('You lose! ','Computer choose: ', random_nr)
+    print('You lose! ',joc[user_nr],'Computer: ',joc[random_nr])
 elif random_nr==1 and user_nr==2:
-    print('You win! ','Computer choose: ', random_nr)
+    print('You win! ',joc[user_nr],'Computer: ',joc[random_nr])
 elif random_nr==2 and user_nr==0:
-    print('You win! ','Computer choose: ', random_nr)
+    print('You win! ',joc[user_nr],'Computer: ',joc[random_nr])
 elif random_nr==2 and user_nr==1:
-    print('You lose! ','Computer choose: ', random_nr)
+    print('You lose! ',joc[user_nr],'Computer: ',joc[random_nr])
 else:
     print('You choose an invalid number. You lose! ')
